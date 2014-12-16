da15e893effdb3780c03de1c6b58b83b49df6de0
Ganesh Bangar <ganesh.bangar@kiprosh.com>
2014-12-16 20:45:45 +0530

    orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,12 +13,24 @@ class OrdersController < ApplicationController
 		end	
 	end
 
-	# def create
-	# 	@order = Order.new(param_values)
-
-	# end
+	def create
+	 	@order = Order.new(param_values)
+	 	@order.add_line_items_from_cart(current_cart)
+	 	respond_to do |format|
+	 		if @order.save
+	 			Cart.destroy(session[:cart_id])
+	 			session[:cart_id] = nil
+				format.html {redirect_to(store_index_path, notice: 'Thank you for your order')}
+ 				format.xml { render xml: @order, status: :created, location: @order }
+			else 
+				format.html {render action: "new"}	
+				format.xml { render xml: @order.errors, status: :unprocessable_entity }
+	 		end
+	 	end	
+	end
 		
-	# def param_values
-	# 	params.require(:order).permit(:name, :address, :email, :pay_type )		
-	# end
+	def param_values
+		params.require(:order).permit(:name, :address, :email, :pay_type )		
+	end
+
 end	
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,8 @@
 class LineItem < ActiveRecord::Base
 belongs_to :product
 belongs_to :cart
+belongs_to :order
+validates :order, presence: true
 	def total_price
 	product.price * quantity
 	end	
